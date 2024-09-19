86efb0259346060d64aa0f67c27f8fc8da22b9a2
Karl Naden <knaden@mitre.org>
2024-09-19 09:38:36 -0400

    linting setup update and run

--- src/scripts/createNarrativeMapping.ts
+++ src/scripts/createNarrativeMapping.ts
@@ -6,16 +6,16 @@ const mapping: Record<string, Record<string, string>> = {};
 const directoryPath = path.join(__dirname, '../../us-core-descriptions/');
 const fileNames = fs.readdirSync(directoryPath);
 fileNames.forEach(file => {
-    const profileName = file.split('descriptions-')[1].replace('.json', '');
-    //eslint-disable-next-line @typescript-eslint/no-var-requires
-    const descriptions = require(
-        path.resolve(path.join(__dirname, '../../us-core-descriptions/', file))
-    ) as Record<string, string>;
-    mapping[profileName] = descriptions;
+  const profileName = file.split('descriptions-')[1].replace('.json', '');
+  //eslint-disable-next-line @typescript-eslint/no-var-requires
+  const descriptions = require(
+    path.resolve(path.join(__dirname, '../../us-core-descriptions/', file))
+  ) as Record<string, string>;
+  mapping[profileName] = descriptions;
 });
 
 const outputPath = path.resolve(
-    path.join(__dirname, '../narrative-mappings/default-narrative-mapping.json')
+  path.join(__dirname, '../narrative-mappings/default-narrative-mapping.json')
 );
 fs.writeFileSync(outputPath, JSON.stringify(mapping, null, 2), 'utf8');
 logger.info(`Wrote output to ${outputPath}`);
